a38a87a50530c35431ad4ef55a6151ddca9d7b37
Manitejanethi <mnethi@marmeto.com>
2025-06-29 23:28:18 +0530

    customer-ic-update

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/mock-api/customer-profile', (req, res) => {
 
   // Simulate quiz results → recommended product IDs
   const mockData = {
-    '23131448443251': [{ id: 52697678578035, handle: 'example-shirt'},{id: 52697678971251, handle:'example-pants'}],
-    '12345678912342': [{ id: 52697678578035, handle: 'example-shirt'},{id: 52697678971251, handle:'example-pants'}],
+    23131448443251 : [{ id: 52697678578035, handle: 'example-shirt'},{id: 52697678971251, handle:'example-pants'}],
+    12345678912342 : [{ id: 52697678578035, handle: 'example-shirt'},{id: 52697678971251, handle:'example-pants'}],
   };
 
-  const recommendations = mockData[customerId] || ['52697678578035']; // default if not found
+  const recommendations = mockData[customerId] || [{ id: 52697678578035, handle: 'example-shirt'}]; // default if not found
   res.json({ recommendations });
 });
 
